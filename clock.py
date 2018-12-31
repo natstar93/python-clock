a56ca8787ef275a4cdad7735f7f5c78f88bfb910
Natalie Akam <natalie.akam@gmail.com>
2018-12-31 00:38:58 +0000

    Get and display Heroku config var

--- clock.py
+++ clock.py
@@ -1,10 +1,13 @@
+import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
+username = os.environ.get('USERNAME')
+
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every three minutes. User {0}'.format(username))
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=20)
 def scheduled_job():
